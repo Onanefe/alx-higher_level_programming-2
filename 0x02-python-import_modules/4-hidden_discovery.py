4c8b91e8cc5a2d016c9e6208e8688a0aca51c74f
Onanefe <106746336+Onanefe@users.noreply.github.com>
2022-09-22 18:24:23 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
-iewkhew
+#!/usr/bin/python3
+import hidden_4
+
+
+def principal():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
+
+if __name__ == "__main__":
+    principal()
